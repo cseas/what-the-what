591001d595e0ee1cc33e13de64c06741a747ba06
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-07-08 00:18:45 +0530

    feat: add search input

--- react-app/src/Home/Home.jsx
+++ react-app/src/Home/Home.jsx
@@ -1,3 +1,22 @@
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
+
 export function Home() {
-  return <p>Home</p>;
+  return (
+    <>
+      <h1 className="h2-blue" style={{ paddingTop: "16px" }}>
+        Wh@t <span className="h2-black">the</span> What!
+      </h1>
+      <main style={{ padding: "16px 24px 0" }}>
+        <InputGroup>
+          <Input
+            variant="flushed"
+            placeholder="Search for your query"
+            focusBorderColor="#4200FF"
+          />
+          <InputRightElement children={<SearchIcon color="#B3B7C5" />} />
+        </InputGroup>
+      </main>
+    </>
+  );
 }
--- react-app/src/index.jsx
+++ react-app/src/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MemoryRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,9 +11,13 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
+    <ChakraProvider resetCSS={false}>
+      {/* <BrowserRouter> */}
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+      {/* </BrowserRouter> */}
+    </ChakraProvider>
   </React.StrictMode>
 );
 
