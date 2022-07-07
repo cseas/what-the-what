c06a95e80a596ce90041c3c0a5e28de338a05498
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-07-07 20:21:45 +0530

    chore: add dummy Home component

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { Login } from "./Login/Login";
+import { Home } from "./Home/Home";
 
 import "./App.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Login />
+      <Home />
     </div>
   );
 }
