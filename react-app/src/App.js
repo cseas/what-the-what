2139260cb2ce4706002bda3654443abed2a61dcc
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-07-07 19:37:15 +0530

    feat: add login page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,48 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>What the What?</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ paddingTop: "40px" }}>
+        <p className="p-bold-light">Welcome to</p>
+
+        <h1>
+          <span className="h1-blue" style={{ paddingRight: "8px" }}>
+            Wh@t
+          </span>
+          <span className="h1-black" style={{ paddingRight: "8px" }}>
+            the
+          </span>
+          <span className="h1-blue">What!</span>
+        </h1>
       </header>
+
+      <main>
+        <img
+          src="/images/home-girl.png"
+          alt="Banner"
+          style={{
+            border: "12px solid hsla(256, 100%, 50%, 0.2)",
+            marginTop: "32px",
+          }}
+        />
+
+        <p className="p-regular" style={{ paddingTop: "32px" }}>
+          Ever wondered <span className="p-bold-blue">What</span> did just
+          someone say in a meeting?
+        </p>
+
+        <p className="p-bold" style={{ paddingTop: "32px" }}>
+          Now you can find the{" "}
+          <span className="p-bold-blue">What's for all your Whats</span> here 😉
+        </p>
+
+        <img
+          src="/images/home-google.png"
+          alt="Log In With Google"
+          style={{ width: "256px", paddingTop: "40px" }}
+        />
+      </main>
     </div>
   );
 }
