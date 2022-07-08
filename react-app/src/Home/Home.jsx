1dd60914200fd5b1a8b65d1af53877f36d8157d6
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-07-08 09:07:39 +0530

    feat: add dummy search results

--- react-app/src/Home/Home.jsx
+++ react-app/src/Home/Home.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+
+import searchResults from "../api/searchResults.json";
 import { SearchResult } from "./SearchResult";
 
 export function Home() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (newInput) => {
+    setSearchInput(newInput.currentTarget.value);
+    console.log(newInput.currentTarget.value);
+  };
+
   return (
     <>
       <h1 className="h2-blue" style={{ paddingTop: "16px" }}>
@@ -12,21 +22,35 @@ export function Home() {
         <InputGroup>
           <Input
             variant="flushed"
-            placeholder="Search for your query"
+            placeholder="Search your question..."
             focusBorderColor="#4200FF"
+            value={searchInput}
+            onChange={handleChange}
           />
           <InputRightElement children={<SearchIcon color="#B3B7C5" />} />
         </InputGroup>
 
         <section style={{ textAlign: "initial" }}>
           <h2 className="h3" style={{ paddingTop: "24px" }}>
-            Un-answered questions (5)
+            Un-answered questions ({searchResults.unansweredQuestions.length})
           </h2>
 
-          <SearchResult
-            problem="What's the full form of MAU?"
-            tags={["BU: X", "General"]}
-          />
+          {searchResults.unansweredQuestions.map(({ problem, tags }) => (
+            <SearchResult problem={problem} tags={tags} />
+          ))}
+        </section>
+
+        <section style={{ textAlign: "initial", paddingTop: "16px" }}>
+          <h2 className="h3" style={{ paddingTop: "24px" }}>
+            Latest answered questions (
+            {searchResults.latestAnsweredQuestions.length})
+          </h2>
+
+          {searchResults.latestAnsweredQuestions.map(
+            ({ problem, tags, answer }) => (
+              <SearchResult problem={problem} tags={tags} answer={answer} />
+            )
+          )}
         </section>
       </main>
     </>
--- react-app/src/Home/SearchResult.jsx
+++ react-app/src/Home/SearchResult.jsx
@@ -1,15 +1,44 @@
+import { Button } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
+
 export function SearchResult({ problem, tags, answer = null }) {
   return (
     <>
       <p className="p-bold" style={{ padding: "16px 0 4px" }}>
         {problem}
       </p>
-      <p>{answer}</p>
-      <div>
-        {tags.map((tag) => (
-          <span className="span-tag">{tag}</span>
-        ))}
-        <button>Add answer</button>
+
+      {answer ? (
+        <p
+          className="p-regular"
+          style={{ color: "hsla(0, 0%, 0%, 0.7)", paddingBottom: "4px" }}
+        >
+          {answer}
+        </p>
+      ) : null}
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          {tags.map((tag) => (
+            <span className="span-tag">{tag}</span>
+          ))}
+        </div>
+
+        <Button
+          colorScheme="purple"
+          variant="link"
+          rightIcon={<ChevronRightIcon />}
+          size="sm"
+          style={{ cursor: "pointer" }}
+        >
+          Add answer
+        </Button>
       </div>
     </>
   );
